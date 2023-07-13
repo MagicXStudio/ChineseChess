56c3a0456edd7d9d9e97dcdb5b474ffb73b2432c
AngkorW <AngkorW@ANGKORW-DEV>
2023-07-13 13:13:11 +0800

    Fixed3D

--- ChineseChess/ChessPiece.Designer.cs
+++ ChineseChess/ChessPiece.Designer.cs
@@ -28,7 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChessPiece
+            // 
+            this.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
